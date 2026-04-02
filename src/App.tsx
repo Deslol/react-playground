6793050b29eabd7a399960a8764a24b1e4884e5b
Cyril Chan <kwunming0911@gmail.com>
2026-04-02 10:18:30 +0100

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
-import './App.css';
+import React, {useReducer} from 'react';
 
 function App() {
-  return (
-    <div>
+    interface Counter {
+        count: number,
+        step: number
+    }
 
-    </div>
-  );
+    type Action = { type: "increment" } | { type: "decrement" } | { type: "reset" } | {
+        type: "setStep",
+        payload: number
+    }
+
+    const initialState = {
+        count: 0,
+        step: 1
+    }
+
+    function reducer(state: Counter, action: Action) {
+        switch (action.type) {
+            case 'increment':
+                return {...state, count: state.count + state.step}
+            case 'decrement':
+                return {...state, count: state.count - state.step}
+            case 'reset':
+                return initialState
+            case 'setStep':
+                return {...state, step: action.payload}
+            default:
+                return state
+        }
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    return (
+        <div className="revise-container">
+            <button onClick={() => dispatch({type: "increment"})}>+</button>
+            <p>{state.count}</p>
+            <button onClick={() => dispatch({type: "decrement"})}>-</button>
+            <button onClick={() => dispatch({type: "reset"})}>Reset</button>
+            <p>Step {state.step}</p> <input value={state.step} type="number" onChange={(e) => dispatch({
+            type: "setStep",
+            payload: Number(e.target.value)
+        })}/>
+        </div>
+    );
 }
 
 export default App;
